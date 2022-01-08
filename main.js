a9aad8bd5033728e63c2158c6805acf58afb6ed8
Chung Songyu <songluck85@hotmail.com>
2022-01-08 17:14:51 +0800

    project carousel: rewrote logic, manual navigation now more responsive

--- main.js
+++ main.js
@@ -16,88 +16,53 @@ $(document).ready(function() {
   $("#h1-14").fadeOut(1).fadeIn(2150);
 
   // ========== carousel ==========
-  let counter = 1;
-  let subCounter = 1;
-  for(let i=1; i<8; i++) {
-    document.getElementById("radio" + i).addEventListener("change", () => {
-      counter = i;
-      subCounter = 1;
-    })
-    document.getElementsByClassName("course-" + i)[0].addEventListener("click", () => {
-      counter = 8 - i;
-      subCounter = 1;
-    })
-  };
-  for(let i=1; i<6; i++) {
-    document.getElementById("radio-sub" + i).addEventListener("change", () => {
-      subCounter = i;
-    })
-  };
+  let course = 7;
+  let project = 5;
 
-  // for later use: max-width > 900px
-  let course = null;
-  let project = null;
+  const projectSelector = () => {
+    // to account for project #5 not being included in carousel
+    if(course == 1 && project == 5) {
+      project = 4;
+    }
 
-  setInterval(() => {
-    document.getElementById("radio" + counter).checked = true;
-    document.getElementById("radio-sub" + subCounter).checked = true;
+    document.getElementById("radio" + course).checked = true;
+    document.getElementById("radio-sub" + project).checked = true;
 
     let x = 0;
-    for(let i=1; i<8; i++) {
+    for(let i=7; i>0; i--) {
       if(document.getElementById("radio" + i).checked) {
-        x -= 4000 * (i - 1);
-        course = i - 1;
+        x -= 4000 * (7 - i);
+        course = i;
       }
     };
-    for(let j=1; j<6; j++) {
+    for(let j=5; j>0; j--) {
       if(document.getElementById("radio-sub" + j).checked) {
-        project = j;
-        if(j > 1) {
-          // to account for project #5 not being included in carousel
-          if(counter == 7 && project == 5) {
-            x = 0;
+        // to account for project #5 not being included in carousel
+        if(course == 1) {
+          if(j == 5) {
+            continue;
           } else {
-            x -= 800 * (j - 1);
+            x -= 800 * (4 - j);
           }
+        } else {
+          x -= 800 * (5 - j);
         }
+        project = j;
       }
     };
-
-    // to account for project #5 not being included in carousel
-    if(course == 7 && project == 1) {
-      project = 2;
-    }
     document.getElementById("first").style.transform = "translateX(" + x.toString() + "px)";
-
-    subCounter++;
-    // to account for project #5 not being included in carousel
-    if(counter == 7 & subCounter == 5) {
-      counter = 1;
-      subCounter = 1;
-    }
-    if(subCounter > 5) {
-      subCounter = 1;
-      counter++;
-    }
-    if(counter > 7) {
-      counter = 1;
-    }
 
     // ========== max-width > 900px ==========
     // check if max-width > 900px
     const element = document.querySelector(".course-7p");
     const notMobile = getComputedStyle(element).display;
     if(notMobile != "none") {
-      let projectId = 36 - (course * 5 + project);
-      // to account for project #5 not being included in carousel
-      if(projectId < 6) {
-        projectId -= 1;
-      }
+      let projectId = (course - 1) * 5 + project;
       $(".project").addClass("hidden-not-mobile");
       $("#project-" + projectId).removeClass("hidden-not-mobile");
 
       // to account for project #5 not being included in carousel
-      if(projectId < 6 && projectId > 0) {
+      if(projectId < 5 && projectId > 0) {
         $(".radio-not-mobile").addClass("hidden");
       } else {
         $(".radio-not-mobile").removeClass("hidden");
@@ -107,13 +72,56 @@ $(document).ready(function() {
       $("#project-3").addClass("br-bottom-left");
       $("#project-2").addClass("br-bottom-left");
       $("#project-1").addClass("br-bottom-left");
-      if(course == 6) {
+      if(course == 1) {
         $(".course-1").removeClass("br-bottom-left");
       } else {
         $(".course-1").addClass("br-bottom-left");
       }
     }
-  }, 4000);
+
+    project--;
+    // to account for project #5 not being included in carousel
+    if(course == 1 & project == 0) {
+      course = 7;
+      project = 5;
+    }
+    if(project < 1) {
+      project = 5;
+      course--;
+    }
+    if(course < 1) {
+      course = 7;
+    }
+  };
+
+  for(let i=7; i>0; i--) {
+    document.getElementById("radio" + i).addEventListener("change", () => {
+      course = i;
+      if(course == 1) {
+        project = 4;
+      } else {
+        project = 5;
+      }
+      projectSelector();
+    })
+    document.getElementsByClassName("course-" + i)[0].addEventListener("click", () => {
+      course = i;
+      if(course == 1) {
+        project = 4;
+      } else {
+        project = 5;
+      }
+      projectSelector();
+    })
+  };
+  for(let i=5; i>0; i--) {
+    document.getElementById("radio-sub" + i).addEventListener("change", () => {
+      project = i;
+      projectSelector();
+    })
+  };
+
+  setInterval(projectSelector, 4000);
 });
 
 $(() => {
